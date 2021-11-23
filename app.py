644fc573e269d2a947a54f59825cf57fb01611ce
andinadila <80771468+andinadila@users.noreply.github.com>
2021-11-23 22:33:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 from multiApp import Multipage
 
-import sys
-sys.path.append("./apps")
-from apps import home, searchEntry, uploadFile, history
-import apps.home
-import apps.searchEntry
-import apps.uploadFile
-import apps.history
+from ../apps import home, searchEntry, uploadFile, history
+# import apps.home
+# import apps.searchEntry
+# import apps.uploadFile
+# import apps.history
 
 import psycopg2
 from configuration import config
